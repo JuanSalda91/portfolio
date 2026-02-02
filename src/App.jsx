c32cdc9ddc3883919ffa5adddfa3a3fb66899399
JuanSalda91 <juansalda9131@gmail.com>
2026-02-02 16:12:32 -0500

    base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
-
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="min-h-screen">
+      <h1 className="text-4xl font-bold text-center py-20 text-primary">
+        Portfolio - Under Construction
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
