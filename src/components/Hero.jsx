2bff79ab2245bcb54c6ae3b750f597b2a76f2274
JuanSalda91 <juansalda9131@gmail.com>
2026-02-03 23:13:25 -0500

    CTA button (projects, contact) added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,6 +30,19 @@ function Hero() {
           interactions to backend APIs, I create scalable, user-focused
           solutions.
         </p>
+
+        {/* CTA Buttons */}
+        <div className="flex flex-col sm:flex-row dap-4 justify-center items-center">
+            {/* Primary CTA - View Projects */}
+            <a href="#projects" className="px-8 py-4 bg-primary text-white font-semibold rounded-lg shadow-lg hover:bg-blue-600 hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300 w-full sm:w-auto">
+                Veiw My Work
+            </a>
+            {/* Secondary CTA - Contact */}
+            <a href="#contact" className="px-8 py-4 bg-white text-primary font-semibold rounded-lg shadow-lg border-2 border-primary hover:bg-primary hover:text-white transform hover:-translate-y-1 transition-all duration-300 w-full sm:w-auto">
+                Get In Touch
+            </a>
+        </div>
+
       </div>
     </section>
   );
