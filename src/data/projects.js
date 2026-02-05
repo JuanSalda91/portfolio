abf55de1ac1be2c7276997c8f985648e8034e5e6
JuanSalda91 <juansalda9131@gmail.com>
2026-02-05 14:09:48 -0500

    2 more projects onjects created with mock information

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,26 @@
 export const projects = [
     {
         id: 1,
+        title: "Invoice Generator",
+        description: "Coming Soon",
+        image: "insert image here",
+        techStack: ["MongoDB", "Express.js", "React", "Node.js"],
+        liveLink: "insert live link here",
+        githubLink: "insert github link here",
+    },
+    {
+        id: 1,
         title: "Inventory/Sales Tracker",
         description: "A complete MERN stack system for managing inventory and sales in real time, with an AI assitant that generates automatic product descriptions, suggest discounts based on low stack levels, and provides sales tends analysis.",
+        image: "insert image here",
+        techStack: ["MongoDB", "Express.js", "React", "Node.js"],
+        liveLink: "insert live link here",
+        githubLink: "insert github link here",
+    },
+    {
+        id: 3,
+        title: "Coming Soon",
+        description: "Coming Soon",
         image: "insert image here",
         techStack: ["MongoDB", "Express.js", "React", "Node.js"],
         liveLink: "insert live link here",
