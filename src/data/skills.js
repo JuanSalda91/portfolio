91a0fb9a0e22ce81e65689770a0996357cc617cd
JuanSalda91 <juansalda9131@gmail.com>
2026-02-05 17:45:44 -0500

    skills data created, ready to export/import to skills component

---
+++ src/data/skills.js
@@ -0,0 +1,25 @@
+export const skills = {
+    frontend: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Tailwind",
+        "React",
+    ],
+    backend: [
+        "Node.js",
+        "Express",
+        "REST APIs",
+    ],
+    database: [
+        "MongoDB",
+        "Mongoose",
+    ],
+    tools: [
+        "VS Code",
+        "Git",
+        "GitHub",
+        "NPM",
+        "Postman",
+    ]
+};
