4c800245a71ffd11db09a9c9efcbd67fc2e983ff
JuanSalda91 <juansalda9131@gmail.com>
2026-02-05 01:21:42 -0500

    first 'project' date created

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Inventory/Sales Tracker",
+        description: "A complete MERN stack system for managing inventory and sales in real time, with an AI assitant that generates automatic product descriptions, suggest discounts based on low stack levels, and provides sales tends analysis.",
+        image: "insert image here",
+        techStack: ["MongoDB", "Express.js", "React", "Node.js"],
+        liveLink: "insert live link here",
+        githubLink: "insert github link here",
+    }
+];
