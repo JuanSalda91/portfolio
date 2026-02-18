15f7500ef49abb66612d0f4e04145c9a05a069ed
JuanSalda91 <juansalda9131@gmail.com>
2026-02-18 01:46:28 -0500

    feat: img added to card

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ export const projects = [
         id: 2,
         title: "Inventory/Sales Tracker",
         description: "A complete MERN stack system for managing inventory and sales in real time, with an AI assitant that generates automatic product descriptions, suggest discounts based on low stack levels, and provides sales tends analysis.",
-        image: "",
+        image: "src/assets/ISTDPRO.png",
         techStack: ["MongoDB", "Express.js", "React", "Node.js"],
         liveLink: "insert live link here",
         githubLink: "https://github.com/JuanSalda91/i.s.t.d-pro-client",
