828fbbd4a21c62feaea8fe274aaf7f52c0e02405
JuanSalda91 <juansalda9131@gmail.com>
2026-03-18 16:58:04 -0400

    feat: basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <section
+      id="footer"
+      className="bg-white dark:bg-dark">
+      <div className="container mx-auto px-4 py-16 text-center">
+        <p>
+            &copy; 2026 [SAL | DEV]. All Rights Reserved.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
